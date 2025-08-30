3151f654473f23d9abe4ff36f0a82d294ad5bfe7
affan1311 <affanhyder6@gmail.com>
2025-08-30 20:57:40 +0500

    prepare for sttaic files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from decouple import config
 import os
 from pathlib import Path
 
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s@bb_n2vp4w^df(7^$#b^b2z*!88f!x@d204=90%e+)n4lxh%0'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true'
+# DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true'
+DEBUG = config('DJANGO_DEBUG',cast = bool)
 print('Django' , DEBUG, type(DEBUG))
 ALLOWED_HOSTS = [
     '.railway.app'
@@ -83,7 +85,18 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config('CONN_MAX_AGE',cast=int,default=30)
+DATABASE_URL = config('DATABASE_URL',cast=str)
 
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -120,6 +133,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_BASE_DIR = BASE_DIR/'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR/'vendors'
+STATICFILES_DIR = [
+    STATICFILES_BASE_DIR
+]
+
+STATIC_ROOT = BASE_DIR / 'local-cdn'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

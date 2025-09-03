3b658bc89eeadf371b11bba92bdd5b80ddf1ae81
affan1311 <affanhyder6@gmail.com>
2025-09-03 22:55:36 +0500

    edit vendor static file

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,6 +142,13 @@ STATICFILES_DIR = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'local-cdn'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
